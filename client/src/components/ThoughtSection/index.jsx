7f9e334d0531fcb8cbbba5dd58ccaf43cdfd9c5e
Stevie Rossetti <rossetti.stephen@gmail.com>
2024-04-18 22:39:42 -0500

    delete, update and readme

--- client/src/components/ThoughtSection/index.jsx
+++ client/src/components/ThoughtSection/index.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
-import { DELETE_THOUGHT } from '../../utils/mutations';
+import { DELETE_THOUGHT, UPDATE_THOUGHT } from '../../utils/mutations';
 
 const ThoughtSection = (course) => {
     console.log(course);
@@ -11,11 +11,22 @@ const ThoughtSection = (course) => {
     })
 
     const [deleteThought] = useMutation(DELETE_THOUGHT);
+    const [updateThought] = useMutation(UPDATE_THOUGHT);
+    const [updatedThought, setUpdatedThought] = useState('');
+
     const handleDelete = async (thoughtId) => {
         try {
             await deleteThought({ variables: { courseId: course.course._id, thoughtId } });
         } catch (error) {
             console.error('Error deleting thought:', error);
+        }
+    };
+
+    const handleUpdate = async (thoughtId, updatedThought) => {
+        try {
+            await updateThought({ variables: { courseId: course.course._id, thoughtId, updatedThought } });
+        } catch (error) {
+            console.error('Error updating thought:', error);
         }
     };
 
@@ -27,6 +38,8 @@ return (
                     <React.Fragment key={thought._id}>
                         <h2 style={{ display: 'inline-block' }}>{thought.thoughtText} - {thought.thoughtAuthor}</h2>
                         <button onClick={() => handleDelete(thought._id)} style={{ marginLeft: '10px', backgroundColor: 'red', color: 'white'  }}>Delete</button>
+                        <input type="text" defaultValue={thought.thoughtText} onChange={(e) => setUpdatedThought(e.target.value)} />
+                        <button onClick={() => handleUpdate(thought._id, updatedThought)}>Update</button>
                         <br />
                     </React.Fragment>
                 ))}
