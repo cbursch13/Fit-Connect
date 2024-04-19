9dcf0f178260c6d8fe77b396463fb6a543bde2e5
cbursch13 <cameron.bursch@target.com>
2024-04-18 23:53:35 -0500

    styling for comments

--- client/src/components/ThoughtSection/index.jsx
+++ client/src/components/ThoughtSection/index.jsx
@@ -5,15 +5,12 @@ const ThoughtSection = (course) => {
     
     return (
         <div className="thought-section">
-            <div className="thought">
-                <h1>Reviews: </h1>
-                {thoughts.map((thought) => (
+                   {thoughts.map((thought) => (
                     <div key={thought._id}>
                         <h2>{thought.thoughtText}</h2>
                         <p>Posted By: {thought.thoughtAuthor}</p>
                     </div>
                 ))}
-            </div>
         </div>
     );
 };
